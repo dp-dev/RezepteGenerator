48215dd828574849ad849a532efaa409dfbde7ff
dp-dev <development@studware.de>
2019-03-17 19:35:14 +0100

    Add further tests

--- src/test/java/de/studware/rezeptegenerator/TestSuite.java
+++ src/test/java/de/studware/rezeptegenerator/TestSuite.java
@@ -5,9 +5,10 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import de.studware.rezeptegenerator.data.RecipeDataTest;
+import de.studware.rezeptegenerator.util.ClipboardGetterTest;
 import de.studware.rezeptegenerator.util.ConfigHandlerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({ GeneratorControllerTest.class, ConfigHandlerTest.class, RecipeDataTest.class, })
+@SuiteClasses({ GeneratorControllerTest.class, ConfigHandlerTest.class, RecipeDataTest.class, ClipboardGetterTest.class })
 public class TestSuite {
 }
